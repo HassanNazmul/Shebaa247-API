5489f101e083201f54e1f29a85d45e1ca2d67f5e
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-12-29 21:00:07 +0000

    Implement custom user model and manager for authentication

--- auth_api/admin.py
+++ auth_api/admin.py
@@ -1,3 +1,40 @@
+# auth_api/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'user_type', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('user_type', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    # Fields shown when editing a user
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('User Type'), {
+            'fields': ('user_type',)
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',
+            ),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    # Fields shown when creating a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ( 'email', 'user_type', 'password1', 'password2',
+            ),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions',)
