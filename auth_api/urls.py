d48a2567276bb39b22d3be3f46a6b97436aa621c
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-12-30 12:13:59 +0000

    Add OTP request and verification endpoints to authentication API

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,7 +1,7 @@
 # auth_api/urls.py
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, RequestOTPView, VerifyOTPView
 
 app_name = 'auth_api'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('otp/request/', RequestOTPView.as_view(), name='request-otp'),
+    path('otp/verify/', VerifyOTPView.as_view(), name='verify-otp'),
 ]
