b1d7acc3148329861683e75be5969ca4efea1bc5
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-12-30 10:57:01 +0000

    Update authentication API URLs

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,11 +1,12 @@
 # auth_api/urls.py
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import LoginView
+from .views import LoginView, RegisterView
 
 app_name = 'auth_api'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
