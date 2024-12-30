b42de3d3a40a771b0ac98c904e8d79ecc7aa5381
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-12-30 10:50:00 +0000

    Update models.py

--- auth_api/models.py
+++ auth_api/models.py
@@ -50,7 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         choices=USER_TYPE_CHOICES
     )
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     objects = CustomUserManager()
